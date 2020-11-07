482ce11d568bda0f75a25f33eaa338a18c6c0058
RobinThielemans <robin.thielemans1@gmail.com>
2020-11-07 15:01:06 +0100

    Magritte-river

---
+++ QuestReadyProject/Assets/DreamForestTree/Script/LookatUser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookatUser : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(2 * transform.position - target.transform.position, transform.up);
+    }
+}
